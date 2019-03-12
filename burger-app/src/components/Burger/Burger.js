8ca910d147d5f2f7a1c7424ebccca6ffffc675ea
Lidii <lidiiany92@gmail.com>
2019-03-12 18:10:32 -0400

    Added message to user

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import classes from './Burger.css';
-import BurgerIngridients from './BurgerIngridients/BurgerIngridients';
+import BurgerIngredients from './BurgerIngridients/BurgerIngridients';
 
 const burger = (props) => {
-    const transformedIngridients = Object.keys(props.ingredients).map(ingredientKey => {
-        return ([...Array(props.ingredients[ingredientKey])].map((_, i) => {
-            return <BurgerIngridients key={ingredientKey + Math.random()} type={ingredientKey} />
-        }));
-    });
+    let transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <BurgerIngredients key={igKey + Math.random()} type={igKey} />;
+            } );
+        } )
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
 
-    console.log(transformedIngridients);
+    console.log(transformedIngredients);
     return (
         <div className={classes.Burger}>
-            <BurgerIngridients type="bread-top"/>
-            {transformedIngridients}
-            <BurgerIngridients type="bread-bottom"/>
+            <BurgerIngredients type="bread-top"/>
+            {transformedIngredients}
+            <BurgerIngredients type="bread-bottom"/>
         </div>
     );
 };
