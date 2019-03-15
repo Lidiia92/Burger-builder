12df0fb8026c55c1a53586fb78d9bf984c4f8bff
Lidii <lidiiany92@gmail.com>
2019-03-15 12:26:18 -0400

    Added buildcontrols and buildcontrol components

---
+++ burger-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const buildControl = (props) => {
+    <div>
+        <div>
+            {props.label}
+            <button>Less</button>
+            <button>More</button>
+        </div>
+    </div>
+}
+
+export default buildControl;
