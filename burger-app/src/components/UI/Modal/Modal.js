190ac72865b604448c3bf3c778667103819c9117
Lidii <lidiiany92@gmail.com>
2019-03-20 16:01:46 -0400

    Added functionality to close the purchase modal menu

--- burger-app/src/components/UI/Modal/Modal.js
+++ burger-app/src/components/UI/Modal/Modal.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import classes from './Modal.css';
+import Aux from '../../../hoc/Wrapper';
+import Backdrop from '../Backdrop/Backdrop';
 
 
 const modal = (props) => (
-    <div className={classes.Modal} style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'}}>
-        {props.children}
-    </div>
+    <Aux>
+        <div className={classes.Modal} style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'}}>
+            {props.children}
+        </div>
+        <Backdrop show={props.show} closeBackdrop={props.closeBackdrop}/>
+    </Aux>
 );
 
 export default modal; 
