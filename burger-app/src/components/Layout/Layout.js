450b8d1c34223a23661d3660f783da1377bb8ab8
Lidii <lidiiany92@gmail.com>
2019-03-25 16:01:57 -0400

    Added Toolbar component

--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Wrapper';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
